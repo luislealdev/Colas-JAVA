d583008849e90742b69cec8d2e0864338a37e272
Luis Leal <luisrrleal@gmail.com>
2022-09-15 17:02:45 -0500

    First commit

---
+++ Cola.java
@@ -0,0 +1,41 @@
+public class Cola {
+    int a_datos[];
+    int a_Inicio, a_Final;
+    int a_contadorDesocupados;
+    Boolean a_error;
+
+    Cola(int p_N) {
+        a_datos = new int[p_N];
+        if (a_Inicio == a_Final) {
+            a_Inicio = a_datos[0];
+            a_contadorDesocupados = p_N;
+        }
+    }
+
+    void m_in(int p_dato) {
+        if (a_contadorDesocupados > 0) {
+            if (a_Final == a_datos.length)
+                a_Final = 0;
+            a_datos[a_Final++] = p_dato;
+            a_contadorDesocupados--;
+        } else
+            a_error = true;
+    }
+
+    int m_out() {
+        if (a_contadorDesocupados != a_datos.length) {
+            int aux;
+            if (a_Inicio == a_datos.length)
+                a_Inicio = 0;
+            aux = a_datos[a_Inicio++];
+            a_contadorDesocupados++;
+            return aux;
+        } else
+            a_error = true;
+        return 0;
+    }
+
+    void m_mostrarDatos() {
+        
+    }
+}
